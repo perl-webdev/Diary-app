5736d8155fb4638d98709e65f61063791733c581
artsiom <moiseevichartsiom@gmail.com>
2023-09-12 23:44:32 +0300

    Added functionality of adding and deleting items

--- my-app/src/components/app/App.js
+++ my-app/src/components/app/App.js
@@ -1,24 +1,64 @@
-import React from "react";
+import React, { Component } from "react";
 import AppHeader from "../app-header";
 import SearchPanel from "../search-panel";
 import PostStatusFilter from "../post-status-filter";
 import PostList from "../post-list";
 import PostAddForm from "../post-add-form";
+import 'react-bootstrap';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faEnvelope, faHeart, faStar, faTrash } from '@fortawesome/free-solid-svg-icons';
 
+export default class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: [
+				{ label: 'Going to learn React', important: false, id: 1 },
+				{ label: 'Going to chill out...', important: false, id: 2 },
+				{ label: 'Meet a friend.', important: false, id: 3 },
+			]
+		};
+		this.deleteitem = this.deleteitem.bind(this);
+		this.addItem = this.addItem.bind(this);
+		this.maxId = 4;
+	}
 
+	deleteitem(id) {
+		this.setState(({ data }) => {
+			const index = data.findIndex(elem => elem.id === id)
+			const newArr = [...data.slice(0, index), ...data.slice(index + 1)];
 
-const App = () => {
-	return (
-		<div>
-			<AppHeader/>
-			<div className='search-panel d-flex'>
-				<SearchPanel/>
-				<PostStatusFilter/>
+			return {
+				data: newArr
+			}
+		});
+	}
+
+	addItem(body) {
+		const newItem = {
+			label: body,
+			important: false,
+			id: this.maxId++
+		}
+		this.setState(({data}) => {
+			const newArr = [...data, newItem];
+			return {
+				data: newArr
+			}
+		})
+	}
+
+	render() {
+		return (
+			<div>
+				<AppHeader />
+				<div className="search-panel d-flex">
+					<SearchPanel />
+					<PostStatusFilter />
+				</div>
+				<PostList posts={this.state.data} onDelete={this.deleteitem} />
+				<PostAddForm onAdd={this.addItem}/>
 			</div>
-			<PostList/>
-			<PostAddForm/>
-		</div>
-	)
+		)
+	}
 }
-
-export default App;
--- my-app/src/components/post-list-item/post-list-item.js
+++ my-app/src/components/post-list-item/post-list-item.js
@@ -1,21 +1,58 @@
-import React from "react";
+import React, { Component } from "react";
+import './post-list-item.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faHeart, faStar, faTrash } from '@fortawesome/free-solid-svg-icons';
 
-const PostListItem = () => {
-	return (
-		<li className="app-list-item d-flex justify-content-between">
-			<span className="app-list-item-label">
-				Hello World!
+export default class PostListItem extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			important: false,
+			like: false
+		};
+		this.onImportant = this.onImportant.bind(this);
+		this.onLike = this.onLike.bind(this);
+	}
+
+	onImportant() {
+		this.setState(({important}) => ({
+			important: !important
+		}))
+	}
+
+	onLike() {
+		this.setState(({like}) => ({
+			like: !like
+		}))
+	}
+
+	render() {
+		const {label, onDelete} = this.props;
+		const {important, like} = this.state;
+		let classNames = 'app-list-item d-flex justify-content-between';
+
+		if (important) {
+			classNames += ' important';
+		}
+		if (like) {
+			classNames += ' like';
+		}
+		
+		return(
+			<div className = { classNames } >
+			<span className="app-list-item-label" onClick={this.onLike}>
+				{label}
 			</span>
 			<div className="d-flex justify-content-center align-items-center">
-				<button className="btn-star btn-sm" type="button">
-					<i className="fa-regular fa-star"></i>
+				<button className="btn-star btn-sm" type="button" onClick={this.onImportant}>
+					<FontAwesomeIcon icon={faStar} />
 				</button>
-				<button className="btn-trash btn-sm" type="button">
-					<i className="fa-solid fa-trash-o"></i>
+				<button className="btn-trash btn-sm" type="button" onClick={onDelete}>
+					<FontAwesomeIcon icon={faTrash} />
 				</button>
+				<FontAwesomeIcon icon={faHeart} className="fa-heart"/>
 			</div>
-		</li>
-	)
-}
-
-export default PostListItem;
+		</div>
+		)
+	}
+};
